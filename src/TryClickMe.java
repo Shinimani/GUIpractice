f43c4a9e976ba99d4e7080a7b6e3a6fa118f1b36
Aayush <aayush.somani111@gmail.com>
2019-04-08 20:20:32 +0530

    Filled the createview method

--- src/TryClickMe.java
+++ src/TryClickMe.java
@@ -1,9 +1,43 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class TryClickMe extends JFrame {
 
+    private JButton buttonCounter, buttonReset;
+    private JLabel labelCount;
+    private int clicks = 0;
+
+
     public TryClickMe()
     {
+        createView();
+
+        setTitle("Clicking");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        pack();//packs everything tightly together to just allow the person to see
+
+    }
+
+    //It's not the best practice to add the components directly to the Jframe so you should use a Jpanel
+    private void createView() {
+
+        JPanel panel = new JPanel();
+        getContentPane().add(panel);
+
+        labelCount = new JLabel();
+        labelCount.setPreferredSize(new Dimension(200,30));
+        panel.add(labelCount);
+
+        buttonCounter = new JButton("Click me");
+        panel.add(buttonCounter);
+
+        buttonReset = new JButton("Reset");
+        panel.add(buttonReset);
+
+
+
 
     }
 
